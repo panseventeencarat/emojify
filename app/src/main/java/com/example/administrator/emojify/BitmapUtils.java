7ebbfb02fcb59405d901ce50d87e2732eb2ca79e
panseventeencarat <452931840@qq.com>
2019-04-24 11:08:55 +0800

    a

--- app/src/main/java/com/example/administrator/emojify/BitmapUtils.java
+++ app/src/main/java/com/example/administrator/emojify/BitmapUtils.java
@@ -11,8 +11,9 @@ import android.util.DisplayMetrics;
 import android.view.WindowManager;
 import android.widget.Toast;
 
+import com.example.administrator.emojify.R;
+
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -20,131 +21,160 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-/**
- * Created by Administrator on 2019/4/15.
- */
- class BitmapUtils {
-    private static final String FILE_PROVIDER_AUTHORITY = "com.example.android.fileprovider";//provider的类名,表示授予 URI 临时访问权限
+class BitmapUtils {
+
+    private static final String FILE_PROVIDER_AUTHORITY = "com.example.android.fileprovider";
+
+
     /**
-     * Resamples 方法会获取设备屏幕的高度和宽度（用像素表示），并重新取样传入的图片以与屏幕大小相符。
+     * Resamples the captured photo to fit the screen for better memory usage.
      *
-     * @param context   :应用程序上下文
-     * @param imagePath .要重新采样照片的路径
-     * @return 重新取样传入与屏幕大小相符的图片
+     * @param context   The application context.
+     * @param imagePath The path of the photo to be resampled.
+     * @return The resampled bitmap
      */
     static Bitmap resamplePic(Context context, String imagePath) {
-        //获取设备屏幕的高度和宽度信息
-        DisplayMetrics metrics=new DisplayMetrics();
-        WindowManager manager=(WindowManager) context.getSystemService(Context.WINDOW_SERVICE);//获取WindowManager服务
-        manager.getDefaultDisplay().getMetrics(metrics);//getDefaultDisplay() 方法将取得的宽高维度存放于DisplayMetrics 对象中,将当前窗口的一些信息放在DisplayMetrics类中，
-        int targetH=metrics.heightPixels;//屏幕高度像素
-        int targetW=metrics.widthPixels;//屏幕宽度像素
-        //获取原始图像的尺寸
-        BitmapFactory.Options bmOptions=new BitmapFactory.Options();
-        bmOptions.inJustDecodeBounds=true;//并不会真的返回一个Bitmap给你，它仅仅会把它的宽，高取回来给你
-        BitmapFactory.decodeFile(imagePath,bmOptions);//获取图片路径，图片高宽
-        int photoW=bmOptions.outHeight;//图片高度
-        int photoH=bmOptions.outWidth;//图片宽度
-       //决定图像的缩放
-        int scaleFactor=Math.min(photoW/targetW,photoH/targetH);//min() 方法可返回指定的数字中带有最低值的数字
-       //将图像文件解码为位图大小以填充视图
-        bmOptions.inJustDecodeBounds=false;//BitmapFactory返回bitmap
-        bmOptions.inSampleSize=scaleFactor;
+
+        // Get device screen size information
+        DisplayMetrics metrics = new DisplayMetrics();
+        WindowManager manager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        manager.getDefaultDisplay().getMetrics(metrics);
+
+        int targetH = metrics.heightPixels;
+        int targetW = metrics.widthPixels;
+
+        // Get the dimensions of the original bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(imagePath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
 
         return BitmapFactory.decodeFile(imagePath);
     }
+
     /**
-     * Creates 该方法会在外部缓存目录下创建临时文件，并返回新的临时文件.
+     * Creates the temporary image file in the cache directory.
      *
-     * @return 返回临时图片文件
+     * @return The temporary image file.
      * @throws IOException Thrown if there is an error creating the file
      */
-    static File createTempImageFile(Context context) throws IOException{
-        String timeStamp=new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+    static File createTempImageFile(Context context) throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+                Locale.getDefault()).format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = context.getExternalCacheDir();//SDCard/Android/data/你的应用包名/cache/目录，一般存放临时缓存数据
-        return File.createTempFile(imageFileName,".jpg",storageDir );//前缀，后缀，目录
+        File storageDir = context.getExternalCacheDir();
+
+        return File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
     }
+
     /**
-     * 该方法尝试删除传入路径下的图片。如果删除失败，则显示 Toast 消息。
+     * Deletes image file for a given path.
      *
-     * @param context   返回临时图片文件
-     * @param imagePath 被删除照片的路径
+     * @param context   The application context.
+     * @param imagePath The path of the photo to be deleted.
      */
-    static boolean deleteImageFile(Context context,String imagePath){
-        //获得图片路径
-        File imageFile=new File(imagePath);
-        //删除图片
-        boolean deleted=imageFile.delete();
-        //若删除失败则弹出弹框
-        if (!deleted){
-            String errorMessage=context.getString(R.string.error);
-            Toast.makeText(context,errorMessage,Toast.LENGTH_SHORT).show();
+    static boolean deleteImageFile(Context context, String imagePath) {
+        // Get the file
+        File imageFile = new File(imagePath);
+
+        // Delete the image
+        boolean deleted = imageFile.delete();
+
+        // If there is an error deleting the file, show a Toast
+        if (!deleted) {
+            String errorMessage = context.getString(R.string.error);
+            Toast.makeText(context, errorMessage, Toast.LENGTH_SHORT).show();
         }
+
         return deleted;
     }
-    //该方法让图片内容提供器将传入路径下的图片添加到系统图库中，以便其他应用能查找该图片。仅在下文所述的 saveImage() 中被调用。
+
+    /**
+     * Helper method for adding the photo to the system photo gallery so it can be accessed
+     * from other apps.
+     *
+     * @param imagePath The path of the saved image
+     */
     private static void galleryAddPic(Context context, String imagePath) {
-        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);//程序通过发送下面的Intent启动MediaScanner服务扫描指定的文件
-        File f = new File(imagePath);//图片文件路径
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(imagePath);
         Uri contentUri = Uri.fromFile(f);
         mediaScanIntent.setData(contentUri);
         context.sendBroadcast(mediaScanIntent);
     }
 
+
     /**
-     * 将传入的位图保存到外部存储器中，放入叫做“Emojify”的子目录下。它还会通过调用上述 galleryAddPic() 将该图片添加到系统图库中
+     * Helper method for saving the image.
      *
      * @param context The application context.
-     * @param image   被保存的图片
-     * @return 被保存图片的路径
+     * @param image   The image to be saved.
+     * @return The path of the saved image.
      */
-    static String saveImage(Context context,Bitmap image){
-        String savedImagePath=null;
-     //在外部存储中创建新文件
-        String timeStamp=new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_" + ".jpg";
-        File storageDir =new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)+"/Emojify");
-        boolean success=true;
-        if (!storageDir.exists()){
-            success=storageDir.mkdirs();
+    static String saveImage(Context context, Bitmap image) {
+
+        String savedImagePath = null;
+
+        // Create the new file in the external storage
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+                Locale.getDefault()).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + ".jpg";
+        File storageDir = new File(
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+                        + "/Emojify");
+        boolean success = true;
+        if (!storageDir.exists()) {
+            success = storageDir.mkdirs();
         }
-        //保存新图像
-        if (success){
-           File imageFile=new File(storageDir,imageFileName);
-            savedImagePath=imageFile.getAbsolutePath();
+
+        // Save the new Bitmap
+        if (success) {
+            File imageFile = new File(storageDir, imageFileName);
+            savedImagePath = imageFile.getAbsolutePath();
             try {
-                OutputStream fOut=new FileOutputStream(imageFile);
-                image.compress(Bitmap.CompressFormat.JPEG,100,fOut);//压缩图像，保留原图像100%的品质
+                OutputStream fOut = new FileOutputStream(imageFile);
+                image.compress(Bitmap.CompressFormat.JPEG, 100, fOut);
                 fOut.close();
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            //将图像添加到系统库
-            galleryAddPic(context,savedImagePath);
-            //在弹窗中显示保存位置
-            String savedMessage=context.getString(R.string.saved_message,savedImagePath);
-            Toast.makeText(context,savedMessage,Toast.LENGTH_SHORT).show();
+
+            // Add the image to the system gallery
+            galleryAddPic(context, savedImagePath);
+
+            // Show a Toast with the save location
+            String savedMessage = context.getString(R.string.saved_message, savedImagePath);
+            Toast.makeText(context, savedMessage, Toast.LENGTH_SHORT).show();
         }
+
         return savedImagePath;
     }
-    /**
 
-     *该方法会创建一个共享隐式 Intent，该 Intent 将调出系统选择器，其中包含处理图片共享的应用。
+    /**
+     * Helper method for sharing an image.
+     *
      * @param context   The image context.
-     * @param imagePath 图片被分享的路径
+     * @param imagePath The path of the image to be shared.
      */
     static void shareImage(Context context, String imagePath) {
-        // 创建一个共享隐式 Intent，开启共享的应用
+        // Create the share intent and start the share activity
         File imageFile = new File(imagePath);
-        Intent shareIntent = new Intent(Intent.ACTION_SEND);//创建共享隐式 Intent调用手机默认分享
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
         shareIntent.setType("image/*");
         Uri photoURI = FileProvider.getUriForFile(context, FILE_PROVIDER_AUTHORITY, imageFile);
         shareIntent.putExtra(Intent.EXTRA_STREAM, photoURI);
         context.startActivity(shareIntent);
     }
-
-
 }
-
-
--- app/src/main/java/com/example/administrator/emojify/MainActivity.java
+++ app/src/main/java/com/example/administrator/emojify/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
@@ -12,7 +13,6 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -28,141 +28,195 @@ import butterknife.OnClick;
 import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
+
+
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     private static final int REQUEST_STORAGE_PERMISSION = 1;
-    private static final String FILE_PROVIDER_AUTHORITY = "com.example.android.fileprovider";//provider的类名,表示授予 URI 临时访问权限
 
-    @BindView(R.id.image_view)ImageView mImageView;//绑定一个view；id为一个view 变量
+    private static final String FILE_PROVIDER_AUTHORITY = "com.example.android.fileprovider";
+
+    @BindView(R.id.image_view) ImageView mImageView;
+
     @BindView(R.id.emojify_button) Button mEmojifyButton;
     @BindView(R.id.share_button) FloatingActionButton mShareFab;
     @BindView(R.id.save_button) FloatingActionButton mSaveFab;
     @BindView(R.id.clear_button) FloatingActionButton mClearFab;
+
     @BindView(R.id.title_text_view) TextView mTitleTextView;
 
     private String mTempPhotoPath;
 
     private Bitmap mResultsBitmap;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);//绑定view
-        Timber.plant(new Timber.DebugTree());  // 创建Timber
+
+        // Bind the views
+        ButterKnife.bind(this);
+
+        // Set up Timber
+        Timber.plant(new Timber.DebugTree());
     }
-    //使用onclick方法让emojifyMebutton启用相机应用
+
+    /**
+     * OnClick method for "Emojify Me!" Button. Launches the camera app.
+     */
     @OnClick(R.id.emojify_button)
-    public void emojifyMe(){
-        //检查是否授予外部存储读写权限
-         if(ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
-             ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},REQUEST_STORAGE_PERMISSION);
-         }//如果未授予权限，则请求权限
-        else{
-             launchCamera();
-         }//如果已授予，就开启相机
+    public void emojifyMe() {
+        // Check for the external storage permission
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // If you do not have permission, request it
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_STORAGE_PERMISSION);
+        } else {
+            // Launch the camera if the permission exists
+            launchCamera();
+        }
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        //当请求外部存储读写权限时调用这个函数,grantResults表示调回结果，requestCode表示请求码
-        switch (requestCode){
-            case REQUEST_STORAGE_PERMISSION:{
-                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    launchCamera();//获得权限，开启相机
-                }
-                else{
-                    Toast.makeText(this,R.string.permission_denied,Toast.LENGTH_SHORT).show();//若获取失败，则跳出弹窗
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        // Called when you request permission to read and write to external storage
+        switch (requestCode) {
+            case REQUEST_STORAGE_PERMISSION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // If you get permission, launch the camera
+                    launchCamera();
+                } else {
+                    // If you do not get permission, show a Toast
+                    Toast.makeText(this, R.string.permission_denied, Toast.LENGTH_SHORT).show();
                 }
                 break;
             }
         }
     }
-    private void launchCamera(){
-        //使用ACTION_IMAGE_CAPTURE隐式intent，实现用本地相机拍照
-        Intent takePictureIntent =new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        //确保有相机来处理intent
-        if (takePictureIntent.resolveActivity(getPackageManager())!=null){
-           //使用BitmapUtils类创建一个临时存放照片的文件
-            File photoFile=null;
-            try{
-                photoFile=BitmapUtils.createTempImageFile(this);
+
+    /**
+     * Creates a temporary image file and captures a picture to store in it.
+     */
+    private void launchCamera() {
+
+        // Create the capture image intent
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the temporary File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = BitmapUtils.createTempImageFile(this);
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                ex.printStackTrace();
             }
-            catch (IOException ex){
-                ex.printStackTrace();//创建文件时出错
-            }
-            //仅在创建文件成功是继续进行操作
-            if(photoFile!=null){
-                //获得临时文件路径
-                mTempPhotoPath=photoFile.getAbsolutePath();
-                //获得图片文件的URI
-                Uri photoURI= FileProvider.getUriForFile(this,FILE_PROVIDER_AUTHORITY,photoFile);
-                //将uri传入intent，使相机可以存储图片
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,photoURI);
-                //从相机获取结果，是否捕获了照片
-                startActivityForResult(takePictureIntent,REQUEST_IMAGE_CAPTURE);
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+
+                // Get the path of the temporary file
+                mTempPhotoPath = photoFile.getAbsolutePath();
+
+                // Get the content URI for the image file
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        FILE_PROVIDER_AUTHORITY,
+                        photoFile);
+
+                // Add the URI so the camera can store the image
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+
+                // Launch the camera activity
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
             }
         }
     }
 
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        //如果以上图像捕获被调用并且成功
-      if (requestCode==REQUEST_IMAGE_CAPTURE && requestCode ==RESULT_OK){
-          //处理图像并把它放到textview中
-          processAndSetImage();
-      }
-        else {
-          //删除临时文件
-          BitmapUtils.deleteImageFile(this,mTempPhotoPath);
-      }
+        // If the image capture activity was called and was successful
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            // Process the image and set it to the TextView
+            processAndSetImage();
+        } else {
+
+            // Otherwise, delete the temporary image file
+            BitmapUtils.deleteImageFile(this, mTempPhotoPath);
+        }
     }
-    //用于处理捕获的图像，并且把它放到Textview中
+
+    /**
+     * Method for processing the captured image and setting it to the TextView.
+     */
     private void processAndSetImage() {
 
-        // 切换视图可见性，把隐藏的浮动的按钮同时显示出来
+        // Toggle Visibility of the views
         mEmojifyButton.setVisibility(View.GONE);
         mTitleTextView.setVisibility(View.GONE);
         mSaveFab.setVisibility(View.VISIBLE);
         mShareFab.setVisibility(View.VISIBLE);
         mClearFab.setVisibility(View.VISIBLE);
 
-        // 对保存的图像重新取样以适应 imageview
+        // Resample the saved image to fit the ImageView
         mResultsBitmap = BitmapUtils.resamplePic(this, mTempPhotoPath);
-        //检测面部并覆盖emoji表情
+
+
+        // Detect the faces and overlay the appropriate emoji
         mResultsBitmap = Emojifier.detectFacesandOverlayEmoji(this, mResultsBitmap);
-        // 将生成的图像位图设置为imageview
+
+        // Set the new bitmap to the ImageView
         mImageView.setImageBitmap(mResultsBitmap);
     }
-    //保存按钮的点击事件
+
+
+    /**
+     * OnClick method for the save button.
+     */
     @OnClick(R.id.save_button)
-    public void saveMe(){
-        //删除临时图片文件
-        BitmapUtils.deleteImageFile(this,mTempPhotoPath);
-        //在外部存储器中保存处理后的图片
-        BitmapUtils.saveImage(this, mResultsBitmap );
+    public void saveMe() {
+        // Delete the temporary image file
+        BitmapUtils.deleteImageFile(this, mTempPhotoPath);
+
+        // Save the image
+        BitmapUtils.saveImage(this, mResultsBitmap);
     }
-//分享按钮的点击事件
+
+    /**
+     * OnClick method for the share button, saves and shares the new bitmap.
+     */
     @OnClick(R.id.share_button)
-    public void shareMe(){
-        //删除临时图片文件
-        BitmapUtils.deleteImageFile(this,mTempPhotoPath);
-        //在外部存储器中保存处理后的图片
-        BitmapUtils.saveImage(this, mResultsBitmap );
-        //分享图片
-        BitmapUtils.shareImage(this,mTempPhotoPath);
+    public void shareMe() {
+        // Delete the temporary image file
+        BitmapUtils.deleteImageFile(this, mTempPhotoPath);
+
+        // Save the image
+        BitmapUtils.saveImage(this, mResultsBitmap);
+
+        // Share the image
+        BitmapUtils.shareImage(this, mTempPhotoPath);
     }
-    //删除按钮的点击事件
+
+    /**
+     * OnClick for the clear button, resets the app to original state.
+     */
     @OnClick(R.id.clear_button)
     public void clearImage() {
-        //清楚照片并改变视图可见性
+        // Clear the image and toggle the view visibility
         mImageView.setImageResource(0);
         mEmojifyButton.setVisibility(View.VISIBLE);
         mTitleTextView.setVisibility(View.VISIBLE);
-        mSaveFab.setVisibility(View.GONE);
         mShareFab.setVisibility(View.GONE);
+        mSaveFab.setVisibility(View.GONE);
         mClearFab.setVisibility(View.GONE);
-        //删除临时图片文件
-        BitmapUtils.deleteImageFile(this,mTempPhotoPath);
 
+        // Delete the temporary image file
+        BitmapUtils.deleteImageFile(this, mTempPhotoPath);
     }
 }
